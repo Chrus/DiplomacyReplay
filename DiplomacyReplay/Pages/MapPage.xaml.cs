49778bcfde54baeece2c02eedcd08a78a411a5dc
Chrus <cdominessy@gmail.com>
2025-01-27 01:28:29 -0500

    -Added Placeholder Pages with sidebars

---
+++ DiplomacyReplay/Pages/MapPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DiplomacyReplay
+{
+    /// <summary>
+    /// Interaction logic for MapPage.xaml
+    /// </summary>
+    public partial class MapPage : Page
+    {
+        private MainWindow main;
+        public MapPage(MainWindow main)
+        {      
+            this.main = main;
+            InitializeComponent();
+        }
+    }
+}
