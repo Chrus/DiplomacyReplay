42775979314b396753a69abc35dafcc0552196d9
Chrus <cdominessy@gmail.com>
2025-01-27 01:33:31 -0500

    Placeholder Pages added to MainWindow HomePage added to MainWindow MainWindow page selector styled

--- DiplomacyReplay/MainWindow.xaml.cs
+++ DiplomacyReplay/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,49 @@ namespace DiplomacyReplay
     /// </summary>
     public partial class MainWindow : Window
     {
+        private HomePage home;
+        private MapPage map;
+        private TimelinePage timeline;
+        private AnimationPage animation;
+
+        public HomePage HomePage { get { return home; } }
+        public MapPage MapPage { get { return map; } }
+        public TimelinePage TimelinePage { get { return timeline; } } 
+        public AnimationPage AnimationPage { get { return animation; } }    
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            home = new HomePage(this);
+            map = new MapPage(this);
+            timeline = new TimelinePage(this);
+            animation = new AnimationPage(this);
+
+            HomeFrame.Content = HomePage;
+            MapFrame.Content = MapPage;
+            TimelineFrame.Content = TimelinePage;
+            AnimationFrame.Content = AnimationPage;
+
+            GoToHomePage();
+        }
+
+        //Listeners
+        private void Home_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            GoToHomePage();
+        }
+
+        //Private Functions
+        private void GoToHomePage()
+        {
+            MainTabControl.SelectedItem = null;
         }
     }
 }
